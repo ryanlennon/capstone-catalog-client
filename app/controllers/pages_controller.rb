e5de1d23fd53c652212cb58c86c01632d03a5d40
ACLTC <acltc@ACLTCs-MacBook-Air-27.local>
2015-12-21 19:22:07 -0600

    added unirest and changed views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,12 @@
 class PagesController < ApplicationController
 
   def show
-    @student = {name: "Jaime", title: "Awesome Capstone", description: "Awesome", image: "jamie.jpg", url: "localhost3000"}
+    @student = Unirest.get("http://172.28.113.208:3000/students.json").body
   end
 
   def index
-    @students = [{name: "Jamie", title: "Awesome Capstone", description: "awesome!!!", image:"http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg", url: "localhost:3000"}, {name: "Ben", title: "Not as Awesome Capstone", description: "totally lame capstone!!!", image:"http://7-themes.com/data_images/out/42/6914793-tropical-beach-images.jpg", url: "localhost:3000"}, {name: "Alex", title: "music related capstone", description: "capstone project about music stuff!!!", image:"http://www.thinkstockphotos.com/CMS/StaticContent/Hero/TS_AnonHP_462882495_01.jpg", url: "localhost:3000"}]
+    @students = Unirest.get("http://172.28.113.208:3000/students.json").body
+
+    # @students = [{name: "Jamie", title: "Awesome Capstone", description: "awesome!!!", image:"http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg", url: "localhost:3000"}, {name: "Ben", title: "Not as Awesome Capstone", description: "totally lame capstone!!!", image:"http://7-themes.com/data_images/out/42/6914793-tropical-beach-images.jpg", url: "localhost:3000"}, {name: "Alex", title: "music related capstone", description: "capstone project about music stuff!!!", image:"http://www.thinkstockphotos.com/CMS/StaticContent/Hero/TS_AnonHP_462882495_01.jpg", url: "localhost:3000"}]
   end
 end
